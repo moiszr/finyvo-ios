0d124e0e2541603bbcbf9f0e560edd3fabd4cb3c
moiszr <moisesnzr.rd@gmail.com>
2025-12-05 22:05:08 -0400

    Initial Commit

---
+++ FinyvoTests/FinyvoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinyvoTests.swift
+//  FinyvoTests
+//
+//  Created by Moises Núñez on 12/5/25.
+//
+
+import Testing
+@testable import Finyvo
+
+struct FinyvoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
